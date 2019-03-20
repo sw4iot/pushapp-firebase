44b01593345475cae0882ea3be93ffed638212fc
newtonjose <josephnewton001@gmail.com>
2019-03-20 20:46:46 -0300

    /update flask

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 from flask import Flask
-from flask_restplus import Api
-from pushapp_firebase import pushapp_firebase
+from flask import request
 
 app = Flask(__name__)
-api = Api(app)
 
-api.add_resource(pushapp_firebase.PushAppFireBase, '/update', endpoint='datareciver')
+# TODO: montar credentials usando volume
+cred = credentials.Certificate('./pushapp_firebase/config/serviceAccount.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+def _send_firebase(data):
+    doc_ref = db.collection(u'app_test').document(u'user')
+    doc_ref.set({
+        u'first': u'Ada',
+        u'last': u'Lovelace',
+        u'born': 1815
+    })
+
+    # result = requests.put(config.FIREBASE_HOST, data=json.dumps(data))
+
+
+@app.route('/update', methods=['POST'])
+def post_update():
+    args = request
+    # data = {
+    #     'nivelMetal': args['nivelMetal'],
+    #     'nivelPapel': args['nivelPapel'],
+    #     'nivelPlastico': args['nivelPlastico'],
+    #     'nivelVidro': args['nivelVidro']
+    # }
+
+    print(args)
+    # _send_firebase(data=args)
+    return "OK", 200
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
